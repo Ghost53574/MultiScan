0ca9b17d862934c0cd1a97393952b3725daf7d2d
gh0st <thelastasylum@gmail.com>
2021-02-24 00:47:04 -0500

    Concurrency fix
    Added the correct way to iterate over completed threads

--- MultiScan.py
+++ MultiScan.py
@@ -16,60 +16,71 @@ class Connect():
     def connect(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.settimeout(1)
-            if not sock.connect_ex((self.IP, self.PORT)):
-                print("{}:{} is open".format(self.IP, self.PORT))  
-            sock.close()
+            result = sock.connect_ex((self.IP, self.PORT))
+            if not result:
+                print("{}:{} open".format(self.IP, self.PORT))
         return False
 
 def Scan(IP, PORT):
     Connect(IP, PORT).connect()
 
 class Threader(object):
-    def __init__(self, ip_list, port_list, WORKERS):
-        self.workers = int(WORKERS)
+    def __init__(self, HOSTS, PORTS):
+        self.ports = PORTS
+        self.hosts = HOSTS
+        self.workers = len(self.hosts)*len(self.ports)
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
             futures = []
-            for ip in ip_list:
-                for port in port_list:
-                    futures.append(
-                    executor.submit(Scan, ip, port)
-                )
+            if self.hosts:
+                for ip in self.hosts:
+                    for port in self.ports:
+                        futures.append(
+                            executor.submit(Scan, ip, port)
+                        )
 
-            for future in concurrent.futures.as_completed(futures):
-                result = future.result()
-                try:
-                    if result != None:
-                        print("{}".format(result))
-                except Exception as e:
-                    print("Error: {}".format(e))
+            completed, not_completed = concurrent.futures.wait(futures)
+            for result in completed:
+                if not result:
+                    print("{}".format(result.result()))
+        
 
-def Port_Range(ports):
-    (start, end) = ports.split('-')
+def Network_Range(NETWORKID):
+    networks = []
+    for network in NETWORKID:
+        hosts = [str(ip) for ip in ipaddress.IPv4Network(network)]
+        for host in hosts:
+            networks.append(host)
+    return networks
+
+def Host_Range(HOSTS):
+    return [str(host) for host in HOSTS]
+
+def Port_Range(PORTS):
+    (start, end) = PORTS.split('-')
     return range(int(start), int(end))
 
-def Ip_Range(networkid):
-    return [str(ip) for ip in ipaddress.IPv4Network(networkid)]
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="MultiScan scans hosts for open ports using multi-threading")
-    parser.add_argument('network', help='network, use the format [network]/[subnet], ex. 10.10.0.0/25 or 192.168.1.15/32', type=str)
-    parser.add_argument('-p', '--ports', help='port list, use the format [start port]-[end port], ex. 20-80. Not specifing uses default range', type=str)
-    parser.add_argument('-w', '--workers', help='workers, specifiy max threads allowed to spawn. Default is 50', type=int)
+    parser = argparse.ArgumentParser(prog='MultiScan', description="MultiScan scans hosts for open ports using multi-threading")
+    required = parser.add_argument_group('Required')
+    required.add_argument('-n', '--network', nargs='+', help='network, use the format [network]/[subnet], ex. 10.10.0.0/25 or 192.168.1.15/32', type=str)
+    required.add_argument('-H', '--hosts', nargs='+', help='host list, specific hosts, ex. 10.10.0.5,10.10.0.123', type=str)
+    parser.add_argument('-p', '--ports', metavar='PORTS', default=None, 
+    help='port list, use the format [start port]-[end port], ex. 20-80. Not specifing uses default range', type=str)
     arguments = parser.parse_args()
 
     PORT_LIST = None
-    IP_LIST = None
-    WORKERS = None
+    HOST_LIST = None
+    network_args = vars(arguments)
 
-    if arguments.hosts:
-        IP_LIST = Ip_Range(arguments.hosts)
+    if network_args['network']:
+        HOST_LIST = Network_Range(network_args['network'])
+    elif network_args['hosts']:
+        HOST_LIST = Host_Range(network_args['hosts'])
+    else:
+        parser.error('Invalid network arguments!')
     if arguments.ports:
         PORT_LIST = Port_Range(arguments.ports)
     else:
         PORT_LIST = Port_Range("0-1023")
-    if arguments.workers:
-        WORKERS = arguments.workers
-    else:
-        WORKERS = 50
-
-    Threader(IP_LIST, PORT_LIST, WORKERS)
+    
+    Threader(HOST_LIST, PORT_LIST)
